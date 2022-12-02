0cbb3c69495a8a301197c616731b7ad942376519
Richard Morris <richardmorris536@gmail.com>
2022-12-02 09:17:47 -0500

    Day 1 done

---
+++ day1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+var lines = File.ReadAllLines("input.txt");
+
+IEnumerable<int> ElfTotals(string[] lines){
+    var sum = 0;
+    foreach(var line in lines)
+    {
+        if(string.IsNullOrWhiteSpace(line))
+        {
+            yield return sum;
+            sum = 0;
+            continue;
+        }
+        sum += Int32.Parse(line);
+    }
+    yield return sum;
+    yield break;
+}
+
+var max = ElfTotals(lines).Max();
+var top3 = ElfTotals(lines)
+    .OrderByDescending(o => o)
+    .Take(3)
+    .Sum();
+
+Console.WriteLine($"Max: {max}");
+Console.WriteLine($"Top 3 Sum: {top3}");
